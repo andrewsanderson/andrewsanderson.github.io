9c49cd3ce572f29ea4cfe0d0425a9b87c80b8795
andrewsanderson <andrewpetersanderson@gmail.com>
2022-12-06 11:15:40 +1100

    hero animation

--- src/Components/Sections/Hero.tsx
+++ src/Components/Sections/Hero.tsx
@@ -1,22 +1,34 @@
 import { MutableRefObject, useEffect, useRef, useState } from "react";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
-  const heroDim = 330;
-
   return (
-    <div className="h-64 flex justify-center my-32 relative w-full mr-8 flex-col-reverse lg:flex-row pt-32 ">
-      <div className="  flex lg:items-end justify-center flex-col grow lg:mr-32 items-center py-6 relative pr-4">
+    <div className=" flex justify-center my-32 relative w-full mr-8 flex-col-reverse lg:flex-row overflow-hidden max-w-[100vw] items-center">
+      <div
+        className={`${styles.title} flex lg:items-end justify-center flex-col grow items-center py-6 relative pr-4`}
+      >
         <div className="text-5xl font-bold text-[#1a3344]">Andrew</div>
         <div className="text-6xl  font-thin pr-2">Sanderson</div>
         <div className="text-xl my-2 text-[#4a6d86]">
           Font-End Web Developer
         </div>
       </div>
-      <div className="flex justify-center items-center">
+      <div
+        className="flex justify-center items-center overflow-hidden relative z-20 bg-[#f9f5ef] h-[350px] w-[350px]"
+        style={{ borderRadius: "50%" }}
+      >
+        <svg
+          className="absolute"
+          height="100%"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <circle className={styles.circle} cx="50" cy="50" r="49.5" />
+        </svg>
         <img
           alt="my-avatar"
           src="/media/me.svg"
-          className="h-[110%] initial lg:absolute border-4 p-1 border-slate-800 shadow-2xl "
+          className={`${styles.avatar} px-[8px] `}
           style={{
             borderRadius: "50%",
             backgroundColor: "#f9f5ef",
@@ -24,11 +36,13 @@ const Hero = () => {
         />
       </div>
       <div
-        className="-z-10 min-w-[40%] h-full grow hidden lg:flex flex-col py-4 "
+        className="-z-10 min-w-[25%] grow hidden lg:flex flex-col py-4 "
         style={{ marginRight: "-3px" }}
       >
         <div className="h-1/2 w-full"></div>
-        <div className="h-1/2 w-full border-t border-dashed border-slate-800"></div>
+        <div
+          className={`${styles.pointer} h-1/2 w-full border-t border-dashed border-slate-800`}
+        ></div>
       </div>
     </div>
   );
